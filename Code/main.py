d174d1bcce49bbb3e774f34c466377416ef0dbd0
Fynn <f.madrian18230@hs-ansbach.de>
2022-06-01 13:08:41 +0200

    added todos

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
-import gym_mairio
 import gym
+import gym_mairio
 import numpy as np
 from helper_functions_python import *
 inputs = {"A": False,
@@ -17,14 +17,19 @@ env = gym.make("mairio-v0")  # Create the environment
 
 def start_simulation():
     state = env.reset()
+    # env.render()
 
     for _ in range(100):
         action = env.action_space.sample()
         action = convert_action(action)
-        # TODO feed data back to observation space
+        print(action)
         state, reward, reset_flag = env.step(action)
         if reset_flag:
             env.reset()
+    # TODO: Test lua and sync
+    # TODO: filter irrelevant blocks
+    # TODO Prevent agent from pressing oppposite directional keys
+    # TODO Fix env.render and state output to 16x14x2 array
 
 
 if __name__ == "__main__":
