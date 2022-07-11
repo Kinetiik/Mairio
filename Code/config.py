810d9e8878c238c8c17dd57fb259382bc4acaca8
QntQ <91533789+QntQ@users.noreply.github.com>
2022-07-11 19:06:38 +0200

    Update config.py

--- Code/config.py
+++ Code/config.py
@@ -1,4 +1,3 @@
 frame_count = 15000  # duration in frame
-gamma = 0.5  # learning parameter, how much of the maximum to include in q_value
 epsilon = 0.1  # amount of random actions
 alpha = 0.01  # learning factor
